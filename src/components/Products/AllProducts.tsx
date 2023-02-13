be0d7e54b2efb7e7e7dc0017216958424d4d5602
Nemanja Malesija <malesija13@gmail.com>
2023-02-13 02:23:17 +0100

    Remove nav sticky when changing page

--- src/components/Products/AllProducts.tsx
+++ src/components/Products/AllProducts.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from 'react';
 import { useProductsContext } from '../../productsContext';
 import './allProducts.css';
 import SortByForm from './SortByForm';
 import ProductsFromApi from './ProductsFromApi';
+import { useObserverContext } from '../../observerContext';
 
 const Products = () => {
   const { filteredProducts } = useProductsContext();
+  const { navRef } = useObserverContext();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    navRef.current.classList.remove('sticky');
+  }, [navRef]);
   return (
     <div className="products-filter-sort-div">
       <div className="products-sort-div">
