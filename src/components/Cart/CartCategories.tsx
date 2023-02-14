06e5bcbd8e9328d748b64cacc75848df8743ed3e
Nemanja Malesija <malesija13@gmail.com>
2023-02-15 00:45:49 +0100

    Finish style cart

--- src/components/Cart/CartCategories.tsx
+++ src/components/Cart/CartCategories.tsx
@@ -3,10 +3,10 @@ import './cart.css';
 const CartCategories= () => {
   return (
     <div className="categories">
-      <h5 className="heading-fifth cat cat-1">Item</h5>
-      <h5 className="heading-fifth cat">Price</h5>
-      <h5 className="heading-fifth cat">Quantity</h5>
-      <h5 className="heading-fifth cat">Subtotal</h5>
+      <h5 className="heading-cat cat-1">Item</h5>
+      <h5 className="heading-cat">Price</h5>
+      <h5 className="heading-cat">Quantity</h5>
+      <h5 className="heading-cat">Subtotal</h5>
     </div>
   );
 };
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,30 +1,15 @@
-import React from 'react';
-
-import { Link } from 'react-router-dom';
 import { useCartContext } from '../cartContext';
-import '../components/Cart/cart.css';
 import CartCategories from '../components/Cart/CartCategories';
 import CartItem from '../components/Cart/CartItem';
+import EmptyCart from '../components/Cart/EmptyCart';
 import TotalAmount from '../components/Cart/TotalAmount';
-
+import '../components/Cart/cart.css';
 const Cart = () => {
   const {
     state: { cart },
   } = useCartContext();
 
-  if (cart.length === 0)
-    return (
-      <section className="section section-cart">
-        <div className="container container-empty">
-          <h2 className="heading-empty-cart">Your cart is empty...</h2>
-          <div className="btn-empty-c-div">
-            <Link to="/products">
-              <button className="btn btn-cart-empty">Keep Shopping</button>
-            </Link>
-          </div>
-        </div>
-      </section>
-    );
+  if (cart.length === 0) return <EmptyCart />;
 
   return (
     <>
