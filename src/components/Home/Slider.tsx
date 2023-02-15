2db22185f6a31c85789ae9c6a8fca5011b8871cf
Nemanja Malesija <malesija13@gmail.com>
2023-02-16 00:51:42 +0100

    Start implementing slider. Add hiearchy and basic styles

--- src/components/Home/Slider.tsx
+++ src/components/Home/Slider.tsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
+import { slides } from '../../utils/Slides';
 import './slider.css';
 
 const Slider = () => {
+  const [activeSlide, setActiveSlide] = useState(1);
+
   return (
     <section className="section-slider">
-      <div className="container container-slider"></div>
+      <div className="container container-slider">
+        <div className="slider">
+          {slides.map((slide, i) => {
+            const { heading, quote, img, location, author } = slide;
+            return (
+              <div className={`slide slide-${i}`}>
+                <div className="testimonial">
+                  <h5 className="heading-testimonial">{heading}</h5>
+                  <blockquote className="quote-testimonial">{quote}</blockquote>
+                  <div className="author-testimonial-div">
+                    <img
+                      src={img}
+                      alt={`slide image ${i + 1}`}
+                      className="image-testimonial"
+                    />
+                    <h6 className="name-testimonial">{author}</h6>
+                    <p className="location-testimonial">{location}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/utils/Slides.tsx
@@ -0,0 +1,38 @@
+export const slides = [
+  {
+    heading: 'Best financial decision ever!',
+    quote:
+      'Lorem ipsum dolor sit, amet conse Accusantium quas quisquam non? Quas voluptate nulla minima dolor sit, amet conse Accusantium quas q dolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas q',
+
+    img: '',
+    author: 'Aarav Lyn',
+    location: 'San Francisco USA',
+  },
+  {
+    heading: 'Lai drazen Lever!',
+    quote:
+      'Lorem ipsum dolor sit, amet conse Accusantium quas quisquam non? Quas voluptate nulla minima dolor sit, amet conse Accusantium quas q dolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas q',
+
+    img: '',
+    author: 'Aarav Lyn',
+    location: 'San Francisco USA',
+  },
+  {
+    heading: 'Strura bura Kleberr!',
+    quote:
+      'Lorem ipsum dolor sit, amet conse Accusantium quas quisquam non? Quas voluptate nulla minima dolor sit, amet conse Accusantium quas q dolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas q',
+
+    img: '',
+    author: 'Aarav Lyn',
+    location: 'San Francisco USA',
+  },
+  {
+    heading: 'Gudura gudura gudura!',
+    quote:
+      'Lorem ipsum dolor sit, amet conse Accusantium quas quisquam non? Quas voluptate nulla minima dolor sit, amet conse Accusantium quas q dolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas qdolor sit, amet conse Accusantium quas q',
+
+    img: '',
+    author: 'Aarav Lyn',
+    location: 'San Francisco USA',
+  },
+];
