67cde38588576e78c69203ef2ddca485545fa36a
Nemanja Malesija <malesija13@gmail.com>
2023-02-15 22:16:09 +0100

    Move tabsContent into separate file, finish style

--- src/components/Home/Tabs.tsx
+++ src/components/Home/Tabs.tsx
@@ -1,39 +1,6 @@
-import { HiOutlineSparkles } from 'react-icons/hi';
-import { FiEye } from 'react-icons/fi';
-import { FiBook } from 'react-icons/fi';
 import { useState } from 'react';
+import { tabsContent } from '../../utils/tabsContent';
 import './tabs.css';
-
-const tabsContent = [
-  {
-    button: 'Swift Delivery',
-    heading: 'Get your home furnishings in a flash with rapid delivery',
-    icon: <HiOutlineSparkles />,
-    paragraph:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, perspiciatis blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia',
-  },
-  {
-    button: 'Stellar Support',
-    heading: 'Always here to help: stellar support around the clock',
-    icon: <FiEye />,
-    paragraph:
-      ' ipsum dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, pLorem blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia',
-  },
-  {
-    button: 'Easy Returns',
-    heading: 'No stress or worries for your home goods',
-    icon: <FiBook />,
-    paragraph:
-      'Explicabo ipsum Error sit ametperspiciati  consectetur voluptatibus elit. Error accusamus arcExplicaboExplictecto minus sint, s blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Exp',
-  },
-  {
-    button: 'Fresh Finds',
-    heading: 'Stay ahead of the trends with our latest  discoveries',
-    icon: <FiEye />,
-    paragraph:
-      ' ipsum dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, pLorem blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia',
-  },
-];
 
 const Tabs = () => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
---
+++ src/utils/tabsContent.tsx
@@ -0,0 +1,35 @@
+import { GiWhirlwind } from 'react-icons/gi';
+import { TbHeartHandshake } from 'react-icons/tb';
+import { RiRefund2Line } from 'react-icons/ri';
+import { MdOutlineDone } from 'react-icons/md';
+
+export const tabsContent = [
+  {
+    button: 'Swift Delivery',
+    heading: 'Get your home furnishings in a flash with rapid delivery.',
+    icon: <GiWhirlwind />,
+    paragraph:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, perspiciatis blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia.',
+  },
+  {
+    button: 'Stellar Support',
+    heading: 'Always here to help: stellar support around the clock.',
+    icon: <TbHeartHandshake />,
+    paragraph:
+      ' Dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, pLorem blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia.',
+  },
+  {
+    button: 'Easy Returns',
+    heading: 'No stress or worries for your home goods.',
+    icon: <RiRefund2Line />,
+    paragraph:
+      'Explicabo ipsum Error sit ametperspiciati  consectetur voluptatibus elit. Error accusamus arcExplicaboExplictecto minus sint, s blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit.',
+  },
+  {
+    button: 'Fresh Finds',
+    heading: 'Stay ahead of the trends with our latest  discoveries.',
+    icon: <MdOutlineDone />,
+    paragraph:
+      ' Ipsum dolor sit amet consectetur adipisicing elit. Error accusamus architecto minus sint, pLorem blanditiis iusto dolor, temporibus aperiam explicabo est ex harum eligendi quia ipsa fugit. Explicabo, voluptatibus officia.',
+  },
+];
