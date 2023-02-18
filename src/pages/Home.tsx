487f1b5169d691bcd300510ede826cbc0aed01cc
Nemanja Malesija <malesija13@gmail.com>
2023-02-18 03:55:54 +0100

    Remove featured products section.  Add responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import Creeds from '../components/Home/Creeds';
-import Featuredin from '../components/Home/FeaturedIn';
+
 import FeaturedProducts from '../components/Home/FeaturedProducts';
 import Hero from '../components/Home/Hero';
 import Slider from '../components/Home/Slider';
@@ -10,7 +9,7 @@ const Home = () => {
   return (
     <>
       <Hero />
-      <Featuredin />
+
       <FeaturedProducts />
       <Creeds />
       <Tabs />
