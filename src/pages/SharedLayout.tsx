7be3928992cbb8176dc9995d26e7135b962045ff
Nemanja Malesija <malesija13@gmail.com>
2023-02-28 22:39:35 +0100

    Remove footer

--- src/pages/SharedLayout.tsx
+++ src/pages/SharedLayout.tsx
@@ -1,13 +1,11 @@
 import Nav from './Nav';
 import { Outlet } from 'react-router-dom';
-import Footer from './Footer';
 
 const SharedLayout = () => {
   return (
     <>
       <Nav />
       <Outlet />
-      <Footer />
     </>
   );
 };
