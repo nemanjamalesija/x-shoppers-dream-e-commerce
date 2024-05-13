ead4732f2c4d1d9f5ef1cdbc8ee3b2855d22618c
Nemanja Malesija <malesija13@gmail.com>
2024-05-13 19:06:51 +0200

    Update urls.ts

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,3 +1,3 @@
-export const allProductsURL = 'https://course-api.com/react-store-products';
+export const allProductsURL = 'https://www.course-api.com/react-store-products';
 
 export const singleProductsURL = `https://course-api.com/react-store-single-product?id=`;
